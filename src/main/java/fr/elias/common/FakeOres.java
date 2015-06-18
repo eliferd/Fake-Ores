19397447234a27ef904a6cd4b0d1db34515f5d3d
elias54 <elias5401@hotmail.fr>
2015-06-19 00:31:47 +0200

    New texture content.

--- src/main/java/fr/elias/common/FakeOres.java
+++ src/main/java/fr/elias/common/FakeOres.java
@@ -16,12 +16,28 @@ public class FakeOres
 	@Instance("fakeores")
 	public static FakeOres instance;
 	
+	public int diamondOre_ID,
+			   emeraldOre_ID,
+			   goldOre_ID,
+			   ironOre_ID,
+			   coalOre_ID,
+			   redstoneOre_ID,
+			   quartzOre_ID,
+			   lapisOre_ID;
+	
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		try {
 			config.load();
-			
+			diamondOre_ID = config.get("Entity", "Diamond Ore Entity", 1010).getInt();
+			emeraldOre_ID = config.get("Entity", "Emerald Ore Entity", 1011).getInt();
+			goldOre_ID = config.get("Entity", "Gold Ore Entity", 1012).getInt();
+			ironOre_ID = config.get("Entity", "Iron Ore Entity", 1013).getInt();
+			coalOre_ID = config.get("Entity", "Coal Ore Entity", 1014).getInt();
+			redstoneOre_ID = config.get("Entity", "Redstone Ore Entity", 1015).getInt();
+			quartzOre_ID = config.get("Entity", "Quartz Ore Entity", 1016).getInt();
+			lapisOre_ID = config.get("Entity", "Lapis Ore Entity", 1017).getInt();
 			config.save();
 		} finally{
 			if(config.hasChanged())
@@ -29,6 +45,7 @@ public class FakeOres
 				config.save();
 			}
 		}
+		
 	}
 	public void init(FMLInitializationEvent event)
 	{
