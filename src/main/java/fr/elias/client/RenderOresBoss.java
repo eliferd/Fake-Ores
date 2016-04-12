7435049a807790837dcf5be42c3601e7c32ae394
Jean-Baptiste <theamateis@gmail.com>
2016-04-12 21:43:03 +0200

    Modifying of Ores Boss Phases render

--- src/main/java/fr/elias/client/RenderOresBoss.java
+++ src/main/java/fr/elias/client/RenderOresBoss.java
@@ -8,40 +8,39 @@ import fr.elias.common.EntityOresBoss;
 import fr.elias.common.FakeOres;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderOresBoss<T extends EntityOresBoss> extends RenderLiving
-{
-	public static final ResourceLocation texture = new ResourceLocation(FakeOres.useOldOresBossModel ? "fakeores:textures/entity/OresBoss2.png" : "fakeores:textures/entity/OresBoss.png");
-	public RenderOresBoss(ModelBase model)
-	{
-		super(Minecraft.getMinecraft().getRenderManager(), model, 2F);
-	}
+public class RenderOresBoss extends RenderLivingBase<EntityOresBoss> {
+
 	private Random rand;
-	public void renderHealthBar(EntityOresBoss mob, double x, double y, double z, float par8, float par9)
-	{
-		if(mob.getPhase() == 2)
-		{ 
+
+	public static final ResourceLocation texture = new ResourceLocation(FakeOres.useOldOresBossModel ? "fakeores:textures/entity/OresBoss2.png" : "fakeores:textures/entity/OresBoss.png");
+
+	public RenderOresBoss(ModelBase modelBoss) {// RenderPlayer
+		super(Minecraft.getMinecraft().getRenderManager(), modelBoss, 2F);
+		this.addLayer(new LayerBossPhases(this, modelBoss));
+	}
+
+	public void renderHealthBar(EntityOresBoss mob, double x, double y, double z, float entityYaw, float partialTicks) {
+		if (mob.getPhase() == 2) {
 			GL11.glColor3f(0.0F, 2.0F, 1.0F);
 		}
-		if(mob.getPhase() == 3)
-		{
+		if (mob.getPhase() == 3) {
 			GL11.glColor3f(2.0F, 1.0F, 2.0F);
 		}
-		super.doRender((EntityLiving)mob, x, y, z, par8, par9);
+		super.doRender(mob, x, y, z, entityYaw, partialTicks);
 	}
-    public void doRender(EntityLiving par1EntityLiving, double par2, double par4, double par6, float par8, float par9)
-    {
-        this.renderHealthBar((EntityOresBoss)par1EntityLiving, par2, par4, par6, par8, par9);
-    }
+
+	public void doRender(EntityLiving par1EntityLiving, double x, double y, double z, float entityYaw, float partialTicks) {
+		this.renderHealthBar((EntityOresBoss) par1EntityLiving, x, y, z, entityYaw, partialTicks);
+	}
+
+	@Override
 	protected ResourceLocation getEntityTexture(EntityOresBoss entity) {
 		return texture;
 	}
-	@Override
-	protected ResourceLocation getEntityTexture(Entity entity) {
-		return getEntityTexture((EntityOresBoss)entity);
-	}
+
 }
